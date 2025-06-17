36d2720c6939c38771240a508f21100bc8458760
Milan Gawade <milangawade028>
2025-06-17 17:10:56 +0530

    Product and inventory management

--- src/main/java/com/example/product/inventory/Dto/ProductDto.java
+++ src/main/java/com/example/product/inventory/Dto/ProductDto.java
@@ -1,5 +1,9 @@
 package com.example.product.inventory.Dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,18 +12,64 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class ProductDto {
 
+    @NotBlank(message ="Product name must not be blank")
+    @Size(min=4,message="Description must not be blank")
     private String productName;
     private String description;
-    private int price;
+
+    @Min(0)
+    @Positive(message="Price must not be zero")
+    private double price;
+
+    @NotBlank(message ="Category must not be blank")
     private String category;
+
+    @NotBlank(message ="Unit must not be blank")
     private String unit;
 
 
-    private boolean isActive;
-    private String createdBy;
-    private LocalDateTime createdDate;
-    private String updatedBy;
-    private LocalDateTime updatedDate;
+    public @NotBlank(message = "Product name must not be blank") @Size(min = 4, message = "Description must not be blank") String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(@NotBlank(message = "Product name must not be blank") @Size(min = 4, message = "Description must not be blank") String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Min(0)
+    @Positive(message = "Price must not be zero")
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(@Min(0) @Positive(message = "Price must not be zero") double price) {
+        this.price = price;
+    }
+
+    public @NotBlank(message = "Category must not be blank") String getCategory() {
+        return category;
+    }
+
+    public void setCategory(@NotBlank(message = "Category must not be blank") String category) {
+        this.category = category;
+    }
+
+    public @NotBlank(message = "Unit must not be blank") String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(@NotBlank(message = "Unit must not be blank") String unit) {
+        this.unit = unit;
+    }
 }
